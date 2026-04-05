c3f5115ad6f3438c986252f5ed79fa7769d53ae9
anujedits42-byte <anujedits42@gmail.com>
2026-04-06 03:38:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-import os
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
 
-API_ID = os.environ.get("API_ID", "34446649")
-API_HASH = os.environ.get("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8772312754:AAEken-freWxFhAHuVZ8iRsUT_oerjz3fT8")
+# Pyrogram setup
+API_ID = "34446649"  # Replace this API ID with your actual API ID
+API_HASH = "8dc570c08d8e35e88fb9bfc73c65d7fa"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8772312754:AAEken-freWxFhAHuVZ8iRsUT_oerjz3fT8"  # Replace this BOT_TOKEN
